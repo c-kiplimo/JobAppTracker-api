3afe8bcbb96b0fe4c3ec4096fca90139b83b3a8c
c-kiplimo <kiplimocollins855@gmail.com>
2024-08-23 19:56:39 +0300

    update

--- src/main/java/com/collicode/jobapp/tracker/api/JobAppTrackerApiApplication.java
+++ src/main/java/com/collicode/jobapp/tracker/api/JobAppTrackerApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobAppTrackerApiApplication {
 
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobAppTrackerApiApplication.class, args);
 	}
